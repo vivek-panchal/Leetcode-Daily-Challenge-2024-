ba883c32848ed01d2edf67d5f4d6008d9a7c9513
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-01-21 14:20:32 +0530

    Create 21 Jan 2024 || 198. House Robber.cpp

---
+++ 21 Jan 2024 || 198. House Robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+       
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1){
+                take+=prev2;
+            }
+            int notTake=prev;
+
+           int curr=max(take,notTake);
+           prev2=prev;
+           prev=curr;
+        }
+
+        return prev;
+    }
+};
