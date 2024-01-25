327a2d685fb5eb7504ff2f9242dca01c245357fb
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-01-25 14:51:58 +0530

    Create 25th Jan 2024 || 1143. Longest Common Subsequence.cpp

---
+++ 25th Jan 2024 || 1143. Longest Common Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(text1[ind1-1]==text2[ind2-1]){
+                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                }
+                else{
+                    dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
