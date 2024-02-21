d7808d958accc1b7233772c595bbfa642640be0e
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-02-21 11:02:20 +0530

    Create 21st Feb 2024 || 201. Bitwise AND of Numbers Range.cpp

---
+++ 21st Feb 2024 || 201. Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int ans = 0;
+        for(int i=31;i>=0;i--){
+            if((m & (1 << i)) != (n & (1 << i))){
+                break;
+            }
+            else{
+                ans |= ((m & (1 << i)));
+            }
+        }
+        return ans;
+    }
+};
