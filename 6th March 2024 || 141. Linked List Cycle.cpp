dc54cff84cfb78b98393d474f3792f37d47a536d
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-03-06 12:23:43 +0530

    Create 6th March 2024 || 141. Linked List Cycle.cpp

---
+++ 6th March 2024 || 141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if ( head==NULL|| head->next==NULL){
+            return false;  
+        }
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(slow!=NULL && fast!=NULL){
+            fast=fast->next;
+            if(fast !=NULL){
+                fast=fast->next;
+            }
+            slow=slow->next;
+
+            if(slow==fast){
+                slow=head;
+                return true;;
+            }
+        }
+
+        return false;
+        
+    }
+};
