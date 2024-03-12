b0a71196b28803feae21ea2caa2583f15b2bd908
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-03-12 11:28:37 +0530

    Create 12th March 2024 || 1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp

---
+++ 12th March 2024 || 1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    ListNode* solve(ListNode* &head,bool &anyChange ){
+        if(head==NULL)
+            return head;
+        ListNode* temp=head;
+        int sum=0;
+        while(temp!=NULL){
+            sum=sum+temp->val;
+            if(sum==0)
+                break;
+            temp=temp->next;
+        }
+        
+        if(sum==0){
+            anyChange=true;
+            return temp->next;
+        }
+        // agli node ko head mankar check karenge prefix sum
+        // return karne ke baad head ke aage join ho jayega answer;
+        head->next=solve(head->next,anyChange);
+        return head;
+        
+    }
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        while(true){
+            bool anyChange = false;
+            head = solve(head,anyChange);
+            
+            if(head==NULL || anyChange==false) break;
+            
+        }
+        return head;
+    }
+};
