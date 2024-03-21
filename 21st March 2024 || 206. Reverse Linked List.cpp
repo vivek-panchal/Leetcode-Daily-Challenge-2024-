0ed76460cc046a85e21a20b1e5885bc04f34bc5d
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-03-21 14:31:32 +0530

    Create 21st March 2024 || 206. Reverse Linked List.cpp

---
+++ 21st March 2024 || 206. Reverse Linked List.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverse(ListNode* &head,ListNode* &curr,ListNode* &prev){
+        if(curr==NULL){
+            head=prev;
+            return;
+        }
+        ListNode* forward=curr->next;
+        reverse(head,forward,curr);
+        curr->next=prev;
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+
+        reverse(head,curr,prev);
+        return head;
+    }
+};
