d6723bafca2159626aa27109dd819cd2017abd55
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-03-22 09:50:36 +0530

    Create 22th March 2024 || 234. Palindrome Linked List.cpp

---
+++ 22th March 2024 || 234. Palindrome Linked List.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+    ListNode* getMid(ListNode* head ) {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        
+        return slow;
+    }
+    ListNode* reverse(ListNode* head) {
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head -> next == NULL) {
+            return true;
+        }
+        
+        //step 1 -> find Middle
+        ListNode* middle = getMid(head);
+        
+        //step2 -> reverse List after Middle
+        ListNode* temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        //step3 - Compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle -> next;
+        
+        while(head2 != NULL) {
+            if(head2->val != head1->val) {
+                return 0;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+    
+        //step4 - repeat step 2
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
+    }
+};
