290d6a5fd010b6c3b542eed50d60cab5e57c13ec
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-06-12 10:52:05 +0530

    Create 12th June 2024 || 75. Sort Colors.cpp

---
+++ 12th June 2024 || 75. Sort Colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n= nums.size();
+        int start=0;
+        int mid=0;
+        int end=n-1;
+        while(mid<=end){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[start]);
+                start++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[end]);
+                
+                end--;
+            }
+        }
+    }
+};
