db2117dfde53f505f8a7077a4bb70c05ec1c5329
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-06-13 17:58:50 +0530

    Create 13th June 2024 || 2037. Minimum Number of Moves to Seat Everyone.cpp

---
+++ 13th June 2024 || 2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+       sort(seats.begin(), seats.end());
+       sort(students.begin(), students.end());
+
+       int moves = 0;
+       for(int i=0; i<students.size(); i++){
+        moves+= abs(students[i] - seats[i]);
+       } 
+
+       return moves;
+    }
+};
