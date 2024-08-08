a32c4b204c716d86757bd4ffdd54064842298b48
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2024-08-08 11:00:57 +0530

    Create 8th Aug 2024 || 885. Spiral Matrix III.cpp

---
+++ 8th Aug 2024 || 885. Spiral Matrix III.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        int steps = 0;
+        int dir = 0;
+        vector<vector<int>> result;
+        result.push_back({rStart, cStart});
+
+        while (result.size() < rows * cols) {
+            if (dir == 0 || dir == 2)
+                steps++;
+
+            for (int i = 0; i < steps; i++) {
+                rStart += directions[dir][0];
+                cStart += directions[dir][1];
+
+                if (rStart >= 0 && rStart < rows && cStart >= 0 &&
+                    cStart < cols) {
+                    result.push_back({rStart, cStart});
+                }
+            }
+
+            dir = (dir + 1) % 4;
+        }
+        return result;
+    }
+};
